6ef98ac8b9aecffbe0b287b47fd71e576f9cf592
Daniela2319 <frankvt04@gmail.com>
2022-08-21 12:53:02 -0300

    livro-receita-media

---
+++ src/LivrosReceitas/LivrosReceitaMedia.java
@@ -0,0 +1,25 @@
+package LivrosReceitas;
+
+
+
+public class LivrosReceitaMedia {
+    public static void main(String[] args) {
+
+        int lPães, lBolos, lPudins, lsalgados, media;
+
+        lPães = 52;
+        lBolos = 39;
+        lPudins = 43;
+        lsalgados = 99;
+
+        media = (lPães+lBolos+lPudins+lsalgados)/4;
+
+        System.out.println("Media de livros de receitas que sao vendidos no Brasil ");
+
+        System.out.println("Media de vendas de livros de receitas sao:  " + media +  " unidades.");
+
+
+    }
+
+
+}
